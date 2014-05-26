a0771a94bfea3f6afe1464b43d94ecb26c093d07
egarza54 <egarza54@gmail.com>
2014-05-26 13:26:13 -0500

    Updated reflection

--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -3,14 +3,97 @@
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
+
+
+
+// Rescue mission
+// move down 2
+// move right 1
+// move up 2
+// move right 2
+// move down 1
+// attack the guard
+
+// Grab the Mushroom
+// move up 1
+// move right 1
+// move left 1
+// move up 1
+// attack the big ogre
+
+// Drink Me
+// move right 1
+// attack the ogre
+// move right 1
+// move down 1
+// move up 1
+// move right 1
+// attack the ogre
+
+//Taunt the guards
+// Move right 1
+// taunt the ogre
+// move left 2
+// tell Phoebe to attack
+// move right 1
+// tell Phoebe to follow you
+// move right 2
+// move up 1
+// move right 1
+// taunt the ogre
+// move down 1
+// move right 1
+// move up 1
+// move right 1
+
+//Its a Trap
+// move down 1 
+// move down 1
+// Call the ogre;
+// Move up 1
+// Move up 1 
+
+//Taunt
+//Taunt the ogre
+//Tell him he's sucks
+//Tell him he's stinks
+//Tell him he's ugly
+
+//Cowardly Taunt
+//move to coordinates (50, 16); 
+//Taunt the ogres toward you;
+//move to coordinates (63, 20);
+//Taunt the ogres
+//Move to a safe spot
+
+ //Commanding Followers
+ //move to coordinates (49, 66);
+ //move to coordinates (60, 63);
+ //move to coordinates(75, 63);
+ //Greet your troops
+ //Tell your troops to follow you
+ //move to coordinates  (65, 44);
+ //move to coordinates  (74, 38);
+ //Tell your troops to attack
+
+ //Mobile Artillery
+ //move to coordinates (37,29)
+ //attack on coordinates (47,7)
+ //move to coordinates (50,31)
+ //attack on coordinates (68,55)
+ //attack on coordinates (60,42)
+ //move to coordinates (49,41)
+ //attack on coordinates (48,64)
+ //attack on coordinates (49,53)
  
- 
- 
- 
- 
- 
- 
- 
- 
+
 // Reflection:
 // Write your reflection here.
+This was a cool exercise. It had me thinking how to approach a solution rather than the problem, making me 
+become proactive in identifying the steps I needed to create the solution. 
+
+
+
+
+
+
